9cec370d1ddbd855171d10f4d8ff0fe5fac235c2
Tom Payne <twpayne@gmail.com>
2024-03-17 21:09:44 +0100

    chore: factor out UNIX code

--- serial.go
+++ serial.go
@@ -2,11 +2,7 @@
 package serial
 
 import (
-	"errors"
-	"os"
 	"time"
-
-	"golang.org/x/sys/unix"
 )
 
 // A Parity is a parity.
@@ -35,31 +31,6 @@ type Config struct {
 	Parity      Parity
 	StopBits    StopBits
 	ReadTimeout time.Duration
-}
-
-// A Port is a serial port.
-type Port struct {
-	file *os.File
-}
-
-// Open opens the serial path at path with the given config.
-func Open(path string, config *Config) (p *Port, err error) {
-	p = &Port{}
-	p.file, err = os.OpenFile(path, unix.O_RDWR|unix.O_NOCTTY, 0)
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		if err != nil {
-			err = errors.Join(err, p.file.Close())
-		}
-	}()
-
-	if err := p.Reconfigure(config); err != nil {
-		return nil, err
-	}
-
-	return p, nil
 }
 
 // Close closes p.
